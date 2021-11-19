4f5d1232f2e02e9a6d9ec4173c11370fbca3067e
alcimarbmx <alcimar_bmx@yahoo.com>
2021-11-19 16:41:31 -0300

    atualização data

--- src/geradoroz/CalendarioFormat.java
+++ src/geradoroz/CalendarioFormat.java
@@ -5,7 +5,6 @@
  */
 package geradoroz;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -19,10 +18,8 @@ public class CalendarioFormat {
     public String SO_gerada() {
         int res = 0;
         Locale.setDefault(new Locale("pt", "BR"));
-        SimpleDateFormat dataf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-        
+        SimpleDateFormat dataf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");        
         Date d = new Date();
-
         String timeAtual = dataf.format(d);
         System.out.println(timeAtual);
         return "" + timeAtual;
@@ -31,67 +28,9 @@ public class CalendarioFormat {
         int res = 0;
         Locale.setDefault(new Locale("pt", "BR"));
         SimpleDateFormat dataf = new SimpleDateFormat(formato);
-        
         Date d = new Date();
-
         String timeAtual = dataf.format(d);
         System.out.println(timeAtual);
         return "" + timeAtual;
     }
-    /*
-    public String DiaAtual_gerada(){
-        int res = 0;
-        Locale.setDefault(new Locale("pt", "BR"));
-        SimpleDateFormat dataf = new SimpleDateFormat("dd/MM/yyyy");
-        
-        Date d = new Date();
-
-        String timeAtual = dataf.format(d);
-        System.out.println(timeAtual);
-        return "" + timeAtual;
-    }
-    public String Hora(){
-        int res = 0;
-        Locale.setDefault(new Locale("pt", "BR"));
-        SimpleDateFormat dataf = new SimpleDateFormat("HH:mm:ss");
-        
-        Date d = new Date();
-
-        String timeAtual = dataf.format(d);
-        System.out.println(timeAtual);
-        return "" + timeAtual;
-    }
-    public String DataMes(){
-        int res = 0;
-        Locale.setDefault(new Locale("pt", "BR"));
-        SimpleDateFormat dataf = new SimpleDateFormat("MM");
-        
-        Date d = new Date();
-
-        String timeAtual = dataf.format(d);
-        System.out.println(timeAtual);
-        return "" + timeAtual;
-    }
-    public String DataDia(){
-        int res = 0;
-        Locale.setDefault(new Locale("pt", "BR"));
-        SimpleDateFormat dataf = new SimpleDateFormat("dd");
-        
-        Date d = new Date();
-
-        String timeAtual = dataf.format(d);
-        System.out.println(timeAtual);
-        return "" + timeAtual;
-    }
-    public String DataAno(){
-        int res = 0;
-        Locale.setDefault(new Locale("pt", "BR"));
-        SimpleDateFormat dataf = new SimpleDateFormat("yyyy");
-        
-        Date d = new Date();
-
-        String timeAtual = dataf.format(d);
-        System.out.println(timeAtual);
-        return "" + timeAtual;
-    }*/
 }
